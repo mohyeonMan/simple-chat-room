8e1326437f67096ab88724be5275435cacf00044
박지훈 <qkrwlgns0510@gmail.com>
2024-12-09 00:06:13 +0900

    feat : Security enabled

---
+++ src/main/java/com/jhpark/simple_chat_room/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.jhpark.simple_chat_room.security.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.jhpark.simple_chat_room.security.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOriginPatterns(List.of("*"));
+                    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    configuration.setExposedHeaders(List.of("Authorization"));
+                    configuration.setAllowCredentials(true);
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", configuration);
+                    cors.configurationSource(source);
+                })
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/api/users/**",
+                                "/swagger-ui/**", // Swagger UI 접근 허용
+                                "/v3/api-docs/**", // Swagger API 문서 접근 허용
+                                "/swagger-resources/**", // Swagger 자원 접근 허용
+                                "/webjars/**", // Swagger WebJar 파일 접근 허용
+                                "/static/**" // 정적 리소스 접근 허용
+                        ).anonymous()
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // JWT 사용
+                                                                                                               // 시 세션
+                                                                                                               // 비활성화
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
